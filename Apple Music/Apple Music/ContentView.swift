1359ab1c06484293fd3fba08fa39839a861251db
Yasmin Kaline <kaline@MacBook-Pro-de-Kaline.local>
2023-05-25 09:59:41 -0300

    adicionando e mudando fundo

--- Apple Music/Apple Music/ContentView.swift
+++ Apple Music/Apple Music/ContentView.swift
@@ -1,47 +1,71 @@
 import SwiftUI
 import UIKit
 
-
 let musicController = MusicController()
-
-
 struct ContentView: View {
     @State var isPaused = true
     @State private var backgroundColor: UIColor = .clear
-    private let image = "backgroundImage"
+    var image = ["imagem1", "imagem2", "imagem3"]
+    @State var index = 0
 
     var body: some View {
         ZStack {
-            Color(backgroundColor).ignoresSafeArea()
-            VStack {
-            HStack(spacing: 76){
-                Button (action: {musicController.playMusic()}){
-                    Image(systemName: "backward.fill")
-                        .resizable()
-                        .frame(width: 53, height: 40, alignment: .center)
-                }
-                if(isPaused == true){
-                    Button (action: {musicController.playMusic(); isPaused = false}){
-                        Image(systemName: "play.fill")
-                            .resizable()
-                            .frame(width: 35, height: 45, alignment: .center)
-                    }
-                }else{
-                    Button (action: {musicController.pauseMusic(); isPaused = true}){
-                        Image(systemName: "pause.fill")
-                            .resizable()
-                            .frame(width: 35, height: 35, alignment: .center)
-                    }
-                }
-                Button (action: {musicController.playMusic()}){
-                    Image(systemName: "forward.fill")
-                        .resizable()
-                        .frame(width: 53, height: 40, alignment: .center)
-                }
+            //background
+            ZStack{
+                Image(image[index])
+                  .blur(radius:20.0)
+                  .aspectRatio(contentMode: .fill)
+                  .edgesIgnoringSafeArea(.all)
+                Color(backgroundColor).ignoresSafeArea()
+                    .opacity(0.8)
+                Rectangle()
+                    .foregroundColor(Color.black)
+                    .opacity(0.4)
             }
-            
-        }.padding()
-
+            VStack {                
+                HStack(spacing: 76){
+                    Button (action: {
+                        if index != 0{
+                            index -= 1;
+                            setAverageColor()
+                        }
+                        musicController.playMusic(index: index)
+                        
+                    }){
+                        Image(systemName: "backward.fill")
+                            .resizable()
+                            .frame(width: 53, height: 40, alignment: .center)
+                    }
+                    if(isPaused == true){
+                        Button (action: {musicController.playMusic(index: index); isPaused = false}){
+                            Image(systemName: "play.fill")
+                                .resizable()
+                                .frame(width: 35, height: 45, alignment: .center)
+                        }
+                    }else{
+                        Button (action: {musicController.pauseMusic(); isPaused = true}){
+                            Image(systemName: "pause.fill")
+                                .resizable()
+                                .frame(width: 35, height: 35, alignment: .center)
+                        }
+                    }
+                    Button (action: {
+                        if index == 2{
+                            index = 0
+                        }else{
+                            index += 1;
+                        }
+                        setAverageColor()
+                        musicController.playMusic(index: index)
+                        
+                    }){
+                        Image(systemName: "forward.fill")
+                            .resizable()
+                            .frame(width: 53, height: 40, alignment: .center)
+                    }
+                }
+                
+            }.padding()
         }.edgesIgnoringSafeArea(.all)
             .onAppear {self.setAverageColor()}
         
@@ -49,7 +73,8 @@ struct ContentView: View {
     
     //Devolve a cor média
     private func setAverageColor() {
-        let uiColor = UIImage(named: image)?.averageColor
+        @State var uiColor = UIImage(named: image[index])?.averageColor
+        print(image[index])
         backgroundColor = uiColor ?? .clear
     }
 }
--- Apple Music/Apple Music/MusicController.swift
+++ Apple Music/Apple Music/MusicController.swift
@@ -4,15 +4,17 @@ import AVFoundation
 class MusicController: UIViewController {
 
     var audioPlayer: AVAudioPlayer?
+    var listaDeMusicas = ["musica1", "musica2", "musica3"]
     
-    @objc func playMusic() {
-        guard let url = Bundle.main.url(forResource: "musica", withExtension: "mp3") else {
+    @objc func playMusic(index: Int) {
+        guard let url = Bundle.main.url(forResource: listaDeMusicas[index], withExtension: "mp3") else {
             print("Erro ao carregar o arquivo de música")
             return
         }
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: url)
             audioPlayer?.play()
+            print(listaDeMusicas[index])
         } catch {
             print("Erro ao reproduzir a música: \(error.localizedDescription)")
         }
